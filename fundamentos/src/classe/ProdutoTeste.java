1f313361aa283a62265082bcae18791ec7ead2b2
CiA dXs TeRrOrIsTaS <annemottadasilva@gmail.com>
2020-09-14 10:38:19 -0300

    Metodos Classe produtos

--- fundamentos/src/classe/ProdutoTeste.java
+++ fundamentos/src/classe/ProdutoTeste.java
@@ -5,19 +5,18 @@ public class ProdutoTeste {
 		Produto p1 = new Produto();	
 		p1.nome = "Notebook";
 		p1.preco = 4700;
-		p1.desconto = 0.30;
-		
+		p1.desconto = 0.1;
 		
 		Produto p2 = new Produto();
 		p2.nome = "Celular";
 		p2.preco = 1700;
-		p2.desconto = 0.25;
+		p1.desconto = 0.1;
 		
-		double precoFinal1 = p1.preco * (1 - p1.desconto); 
-		double precoFinal2 = p2.preco * (1 - p1.desconto); 
-		double mediaCarrinho = (precoFinal1 + precoFinal2) / 2;
+		double precoFinal1 = p1.precoComDesconto();
+		double precoFinal2 = p2.precoComDesconto();
+		double carrinho = (precoFinal1 + precoFinal2);
 
-		System.out.printf("Média do carrinho = R$%.2f.", mediaCarrinho);
+		System.out.printf("Média do carrinho = R$%.2f.",carrinho);
 		
 	}
 }
