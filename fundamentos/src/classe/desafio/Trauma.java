60611747640f0e0c756b9a1a4b4c13fd09c0d5c6
CiA dXs TeRrOrIsTaS <annemottadasilva@gmail.com>
2020-09-15 21:54:08 -0300

    Desafio - acessar primitivos static e de instancia1

---
+++ fundamentos/src/classe/desafio/Trauma.java
@@ -0,0 +1,12 @@
+package classe.desafio;
+
+public class Trauma {
+	int a = 9;
+	static int b = 5;
+	public static void main(String[] args) {
+		Trauma t = new Trauma();
+		System.out.println(t.a);
+		
+		System.out.println(b);
+	}
+}
