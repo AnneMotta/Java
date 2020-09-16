ade970b32196fc268fac25d806ea391d8844360b
CiA dXs TeRrOrIsTaS <annemottadasilva@gmail.com>
2020-09-16 19:55:01 -0300

    estrutura de controle foreach

---
+++ arrays_e _collections/src/arrays/ForEach.java
@@ -0,0 +1,10 @@
+package arrays;
+
+public class ForEach {
+	public static void main(String[] args) {
+		double[] notas = {7.7, 8.6, 9.3, 5.9,8.1};
+		for (double nota : notas) {
+			System.out.println(nota);
+		}
+	}
+}
---
+++ arrays_e _collections/src/arrays/desafio/Array_2.java
@@ -0,0 +1,17 @@
+package arrays.desafio;
+
+import java.util.Arrays;
+
+public class Array_2 {
+
+	public static void main(String[] args) {
+		double[] notasAluna = {9.5, 8.6, 7.4};
+		double total_notas_aluna = 0;
+		for (double notas : notasAluna) {
+			total_notas_aluna += notas;
+			
+		}
+		System.out.println(Arrays.toString(notasAluna));
+		System.out.println(total_notas_aluna / notasAluna.length);
+	}
+}
