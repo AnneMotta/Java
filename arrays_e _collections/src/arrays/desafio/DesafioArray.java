3a2981a5d763b88fc78dc7326ac76ac2981d47e7
CiA dXs TeRrOrIsTaS <annemottadasilva@gmail.com>
2020-09-17 13:23:21 -0300

    Desafio fragmentado com for e foreach

--- arrays_e _collections/src/arrays/desafio/DesafioArray.java
+++ arrays_e _collections/src/arrays/desafio/DesafioArray.java
@@ -1,5 +1,31 @@
 package arrays.desafio;
 
-public class DesafioArray {
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class DesafioArray {
+	public static void main(String[] args) {
+		//programa que captura a quantidade de notas que o aluno irá fornecer, soma a notas e imprime resultado
+		
+		
+		System.out.print("Digite a quantidade de notas a serem informadas: ");
+		Scanner input = new Scanner(System.in);
+		int quantidade_notas = input.nextInt();
+		double[] notas = new double [quantidade_notas];
+		
+		for(int i = 0; i<quantidade_notas; i++) {
+			System.out.print("Digite sua nota: ");
+			notas[i] = input.nextDouble();
+		}
+		
+		System.out.println("Notas informadas: " + Arrays.toString(notas));
+		double total_notas = 0;
+		for (double nota : notas) {
+			total_notas += nota;
+		}
+		
+		double media = total_notas/ notas.length;
+		System.out.println("Sua média é " + media); 
+		input.close();
+	}
 }
