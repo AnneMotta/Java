43e8f5247f5b1771f1e9905679f4e25fcebafb1b
CiA dXs TeRrOrIsTaS <annemottadasilva@gmail.com>
2020-09-23 17:55:22 -0300

    HashSet

--- arrays_e _collections/src/collections/ConjuntoBagunçado.java
+++ arrays_e _collections/src/collections/ConjuntoBagunçado.java
@@ -1,5 +1,48 @@
 package collections;
 
-public class ConjuntoBagunçado {
+import java.util.HashSet;
+import java.util.Set;
 
+public class ConjuntoBagunçado {
+	
+	public static void main(String[] args) {
+		
+		HashSet conjunto = new HashSet();
+		conjunto.add(1);
+		conjunto.add("valores");
+		conjunto.add(2.8);
+		conjunto.add(false);
+		System.out.println(conjunto.size());
+		
+		conjunto.add(1);
+		System.out.println(conjunto.size());
+		
+		conjunto.remove(true);
+		conjunto.remove(2.8);
+		System.out.println(conjunto.size());
+		
+		Set num = new HashSet();
+		num.add(1);
+		num.add(2);
+		num.add(3);
+		
+		System.out.println(conjunto);
+		System.out.println(num);		
+		
+//		System.out.println(num.retainAll(conjunto));
+//		System.out.println(num);
+		
+		System.out.println(conjunto.addAll(num));
+		System.out.println(conjunto);
+		conjunto.clear();
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
 }
