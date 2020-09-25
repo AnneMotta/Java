dff255fd26518ea05bf4270ab1c60d0225b8277f
CiA dXs TeRrOrIsTaS <annemottadasilva@gmail.com>
2020-09-25 16:23:56 -0300

    Reuso via composicao, relacionamento um para um, bidirecional

---
+++ orientacao_objeto/src/composicao/produtos/bidirecional/CompraTest.java
@@ -0,0 +1,15 @@
+package composicao.produtos.bidirecional;
+
+public class CompraTest {
+		
+	public static void main(String[] args) {
+	
+		Compra compra_um = new Compra();
+		compra_um.cliente = "Anne Motta";
+		compra_um.adicionarItem("Livro", 2, 89.50);
+		compra_um.adicionarItem("Caderno", 3, 50.98);
+		compra_um.adicionarItem(new Item ("Caneta", 5, 23.74));
+		
+		System.out.println(compra_um.getValorTotal());
+	}
+}
