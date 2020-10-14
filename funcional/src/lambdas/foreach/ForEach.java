3b2f12f10e838200dc8c950c034cd9ca2d03dad2
CiA dXs TeRrOrIsTaS <annemottadasilva@gmail.com>
2020-10-14 17:34:17 -0300

    percorrendo foreach utilizando metodo static, lambdas e metod references

--- funcional/src/lambdas/foreach/ForEach.java
+++ funcional/src/lambdas/foreach/ForEach.java
@@ -6,13 +6,26 @@ import java.util.List;
 public class ForEach {
 	public static void main(String[] args) {
 		List<String> aprovados = Arrays.asList("Ana", "Cam", "Gui", "Sil" );
+		
 		System.out.println("Forma tradicional:");
 		for(String nome: aprovados) {
 			System.out.println(nome);
 		}
+		
 		System.out.println("\nLambda#1");
 		aprovados.forEach((nome) -> {System.out.println(nome);});
+		
 		System.out.println("\nMetod References");
 		aprovados.forEach(System.out::println);
+		
+		System.out.println("\nLambda#2");
+		aprovados.forEach((nome) -> {Imprimir(nome);});
+		
+		System.out.println("\nMetod References#2");
+		aprovados.forEach(ForEach::Imprimir);
+	}
+	
+	static void Imprimir(String nome) {
+		System.out.println("Olá, meu nome é " + nome);
 	}
 }
